c638ab73b75b2be764941aa1130153b0415970ea
Julien Etienne <julien.etienne@orange.com>
2023-11-08 10:04:49 +0100

    Manage multiple kv

--- kv.go
+++ kv.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	badger "github.com/dgraph-io/badger/v3"
+	badger "github.com/dgraph-io/badger/v4"
 	"github.com/dop251/goja"	
 	"go.k6.io/k6/js/modules"
 )
@@ -28,14 +28,14 @@ var (
 )
 
 type Client struct {
-	vu      modules.VU
+	vu  modules.VU
 	db 	*badger.DB
 }
 
-var check = false
-var client *Client
+var clients map[string]*Client
 
 func init() {
+	clients = make(map[string]*Client)
 	modules.Register("k6/x/kv", new(KV))	
 }
 
@@ -59,35 +59,52 @@ func (mi *ModuleInstance) Exports() modules.Exports {
 }
 
 // NewClient is the JS constructor for the Client
+// Arguments :
+// 	1 arg :  kv_name
+//	2 args : kv_name, filename
+// If filename="" then memory=true
 func (mi *ModuleInstance) NewClient(call goja.ConstructorCall) *goja.Object {	
 	rt := mi.vu.Runtime()
 
 	var name string = ""
+	var kvName string = ""
 	var memory bool = false
+
 	if len(call.Arguments) == 1 {
-		name = call.Arguments[0].String()		
+		kvName = call.Arguments[0].String()	
+		//fmt.Printf("call.Arguments=%i, kvname=%s\n", call.Arguments, kvName)
+		memory=true	
 	}
 
 	if len(call.Arguments) == 2 {
-		name = call.Arguments[0].String()
-		memory = call.Arguments[1].ToBoolean()		
+		kvName = call.Arguments[0].String()
+		name = call.Arguments[1].String()	
+		if name=="" {
+			memory=true
+		}
+		//fmt.Printf("call.Arguments=%i, kvname=%s, filename=%s, memory=%t\n", call.Arguments, kvName, name, memory)	
+	}
+	
+	if kvName==""{
+		kvName="default"
 	}
 
-	if check != true {
-		if name == "" {
-			name = "/tmp/badger"
-		}
+	if client, exists:=clients[kvName];!exists {
 		var db *badger.DB
 		if memory {
 			db, _ = badger.Open(badger.DefaultOptions("").WithLoggingLevel(badger.ERROR).WithInMemory(true))
+			//fmt.Printf("Create badger in memory\n")	
 		} else {
 			db, _ = badger.Open(badger.DefaultOptions(name).WithLoggingLevel(badger.ERROR))
+			//fmt.Printf("Create badger in disk\n")	
 		}
 		client = &Client{vu:mi.vu,db: db}
-		check = true
-	}
+		clients[kvName]=client
 
-	return rt.ToValue(client).ToObject(rt)
+		return rt.ToValue(client).ToObject(rt)	
+	} else {
+		return rt.ToValue(client).ToObject(rt)
+	}
 }
 
 // Set the given key with the given value.
@@ -124,6 +141,76 @@ func (c *Client) Get(key string) (string, error) {
 	}
 	return "", fmt.Errorf("error in get value with key %s", key)
 }
+
+// Pop returns the value for the given key and remove it
+func (c *Client) Pop(key string) (string, error) {
+	var valCopy []byte
+	_ = c.db.Update(func(txn *badger.Txn) error {
+		item, _ := txn.Get([]byte(key))
+		if item != nil {
+			valCopy, _ = item.ValueCopy(nil)
+			_ = txn.Delete([]byte(key))
+		}
+		return nil;
+	})
+	if len(valCopy) > 0 {
+		return string(valCopy), nil
+	}
+	return "", fmt.Errorf("error in get value with key %s", key)
+}
+
+
+func (c *Client) PopFirst() (string, error) {
+	var key []byte
+	_ = c.db.Update(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 10
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		
+		for it.Rewind(); it.Valid(); it.Next() {
+		  item := it.Item()
+		  key := item.Key()
+		  _ = txn.Delete([]byte(key))
+		  fmt.Printf("First() - key=%s\n", key)	
+		  break
+		}
+		return nil
+	  })
+
+	fmt.Printf("First() - check len(key) > 0 key=%s\n", string(key))	
+	//fmt.Printf("First() - len key=%i\n", len(key))	
+
+	if len(key) > 0  {
+		fmt.Printf("First() - len(key) > 0 key=%s\n", key)	
+		return string(key), nil
+	}
+	return "", fmt.Errorf("First() - no data")
+}
+
+// Display the keys - values
+func (c *Client) Show() error {
+	err := c.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 10
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+		  item := it.Item()
+		  k := item.Key()
+		  err := item.Value(func(v []byte) error {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+			return nil
+		  })
+		  if err != nil {
+			return err
+		  }
+		}
+		return nil
+	  })
+	return err
+}
+
 
 // ViewPrefix return all the key value pairs where the key starts with some prefix.
 func (c *Client) ViewPrefix(prefix string) map[string]string {
